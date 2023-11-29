584ad64dd1e6b56e3265169217fdc09d16abc249
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-11-29 23:06:06 +0100

    added item edit & fixed bug

---
+++ src/components/Scrollitems/ScrollitemEdit.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import  { Modal, Input } from "antd"
+
+const { TextArea } = Input;
+
+const ScrollitemEdit: React.FC = ({itemIndex, index, itemNames, setItems}) => {
+
+const [isEditItemOpen, setEditIsItemOpen] = useState<boolean>(false);
+const [newItem, setNewItem] = useState<string>(itemNames[itemIndex]);
+
+  const handleOk = () => {
+    setEditIsItemOpen(false);
+    const newArray = itemNames.with(itemIndex, newItem)
+    setItems(newArray)
+    localStorage.setItem("newItems" + index, JSON.stringify(newArray))
+  };
+
+
+  const handleCancel = () => {
+    setEditIsItemOpen(false);
+  };
+return(
+<>
+<div
+onClick={() => setEditIsItemOpen(true)}
+className="hover:cursor-pointer"
+>
+✒️
+</div>
+
+{isEditItemOpen &&
+<Modal 
+title="Write your task" 
+open={isEditItemOpen} 
+onOk={handleOk} 
+onCancel={handleCancel}
+>
+
+<TextArea 
+      value={newItem}
+      onChange={(e) => setNewItem(e.target.value)}
+      maxLength="15"
+/>
+
+</Modal>
+}
+</>
+)
+}
+
+export default ScrollitemEdit
